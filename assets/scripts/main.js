420ff385bd750aff478a6785dc054f34091efb3e
Kauã de Oliveira <98750284+kauadeoliveira@users.noreply.github.com>
2022-06-01 11:04:38 -0300

    Feat (header): Adicionando novas funções aos botoes do cabecalho
    - Janela Modal no botão "Custom Pomodoro"

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,124 +1,171 @@
-// Variaveis
-let time = 25 * 60
-let displayPomodoro = document.querySelector("#timer");
+// ====== VARIABLES =====
+// -> Timer
+let pomodoro = 25;
+let short = 5;
+let long = 30;
+let time = pomodoro * 60;
 let cron = null;
 
+// -> Display Timer
+let displayPomodoro = document.querySelector("#timer");
 
-let theme = 'dark'
+// -> Theme
+let theme = 'dark';
 
 
+// ===== Functions Timer =====
 
+// -> Update Timer
 function updateTime() {
-    let min = Math.floor(time / 60) 
-    let sec = time % 60 
+    let min = Math.floor(time / 60);
+    let sec = time % 60;
     time > 0 ? time-- : time = 0
-
-
 
     min < 10? min = '0' + min : min = min 
     sec < 10? sec = '0' + sec : sec = sec 
 
     displayPomodoro.innerHTML = `${min}:${sec}` 
-
 }
-updateTime()
+updateTime();
 
+
+
+
+
+// ===== Functions Buttons =====
+// -> Update Buttons "Disabled" / "Enabled"
 function bttnClick(bttnEnable, bttnDisable){
-    document.querySelector(bttnEnable).classList.remove("disabled")
-    document.querySelector(bttnDisable).classList.add("disabled")
+    document.querySelector(bttnEnable).classList.remove("disabled");
+    document.querySelector(bttnDisable).classList.add("disabled");
 }
 
-
-// CLICK PLAY
-$("#playBttn").click(playPomodoro)
+// -> Button PLAY
+$("#playBttn").click(playPomodoro);
 
 function playPomodoro(){
     bttnClick("#pauseBttn", "#playBttn")
     cron = setInterval(() => {
         if(time === 1){
-            document.querySelector("#audioAlarm").play()
+            document.querySelector("#audioAlarm").play();
         }
-        updateTime()
-    }, 1000)
+        updateTime();
+    }, 1000);
 }
 
-
-// CLICK PAUSE
+// -> Button PAUSE
 $("#pauseBttn").click(() => {
-    clearInterval(cron)
-    bttnClick("#playBttn", "#pauseBttn")
+    clearInterval(cron);
+    bttnClick("#playBttn", "#pauseBttn");
 })
 
-
-//CLICK RESET
+// -> Button RESET
 $("#resetBttn").click(() => {
-    pomodoroOption()
+    pomodoroOption();
 })
 
 
-$("#pomodoroBttn").click(pomodoroOption)
 
-$("#shortBreakBttn").click(pomodoroOption)
-
-$("#longBreakBttn").click(pomodoroOption) 
-
-
+// -> Pomodoro Options Button ("Pomodoro", "Short Break", "Long Break")
 function pomodoroOption(){
     if(pomodoroBttn.checked){
-        time = 25 * 60
+        time = pomodoro * 60;
     }
     else if(shortBreakBttn.checked){
-        time = 5 * 60
+        time = short * 60;
     }
     else{
-        time = 30 * 60
+        time = long * 60;
     }
-    updateTime()
+    updateTime();
 }
 
+// -> Click Event
+$("#pomodoroBttn").click(pomodoroOption);
 
+$("#shortBreakBttn").click(pomodoroOption);
+
+$("#longBreakBttn").click(pomodoroOption); 
+
+
+
+
+
+// ===== Pomodoro Custom Modal =====
+
+function pomodoroCustom(){
+    pomodoro = $("#pomodoroTime").val();
+    short = $("#shortTime").val();
+    long = $("#longTime").val();
+    pomodoroOption();
+}
+
+// -> Default Button
+function defaultPomodoroBttn(){
+    $("#pomodoroTime").attr("value", 25);
+    $("#shortTime").attr("value", 5);
+    $("#longTime").attr("value", 30);
+}
+
+// -> Click Event
+$("#defaultPomodoro").click(defaultPomodoroBttn)
+$("#savePomodoro").click(pomodoroCustom);
+
+
+
+
+// ===== DARK / LIGHT MODE =====
+
+// -> Event Click Buttons
 $("#themeBttn").click(() => {
     if(theme === 'dark'){
-        theme = 'light'
-        lightMode()
+        theme = 'light';
+        lightMode();
     }
     else{
-        theme = 'dark'
-        darkMode()
+        theme = 'dark';
+        darkMode();
     }
 })
 
 function lightMode(){
     // Theme Button
-    $("#themeIcon").attr("class", "bi bi-brightness-high-fill mx-1")
-    $("#themeBttn > p").text("Light Mode")
+    $("#themeIcon").attr("class", "bi bi-brightness-high-fill mx-1");
+    $("#themeBttn > p").text("Light Mode");
 
     // Custom Button
-    $("#customIcon").attr("class", "bi bi-gear-fill mx-1")
+    $("#customIcon").attr("class", "bi bi-gear-fill mx-1");
 
     // Logo
-    $("#logo > span").css("color", "black")
+    $("#logo > span").css("color", "black");
 
     // Body / Nav
-    $("body").attr("class", "bg-light")
-    $("nav").removeClass("navbar-dark")
+    $("body").attr("class", "bg-light");
+    $("nav").removeClass("navbar-dark");
+    
+    // Modal
+    $("#modalContent").removeClass("bg-dark text-light").addClass("bg-light text-dark");
+    $(".modal-header > button").removeClass("btn-close-white");
 }
 
 function darkMode(){
     // Theme Button
-    $("#themeIcon").attr("class", "bi bi-moon mx-1 mx-1")
-    $("#themeBttn > p").text("Dark Mode")
+    $("#themeIcon").attr("class", "bi bi-moon mx-1 mx-1");
+    $("#themeBttn > p").text("Dark Mode");
 
     // Custom Button
-    $("#customIcon").attr("class", "bi bi-gear mx-1")
+    $("#customIcon").attr("class", "bi bi-gear mx-1");
 
     // Logo
-    $("#logo > span").css("color", "white")
+    $("#logo > span").css("color", "white");
 
     // Body / Nav
-    $("body").attr("class", "bg-dark")
-    $("nav").addClass("navbar-dark")
+    $("body").attr("class", "bg-dark");
+    $("nav").addClass("navbar-dark");
 
-
+    // Modal
+    $("#modalContent").removeClass("bg-light text-dark").addClass("bg-dark text-light");
+    $(".modal-header > button").addClass("btn-close-white");
 }
+
+
 
